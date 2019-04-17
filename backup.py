191fa3c4a08b174bc4078f90eecae15388015dab
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-04-17 13:15:19 -0400

    Add Backup boilerplate

--- backup.py
+++ backup.py
@@ -2,6 +2,26 @@
 # MariaDB backup/restore helper
 # Copyright 2019, Development Gateway, GPL3+, see LICENSE
 
+class Backup:
+    def __init__(self, directory, parent = None):
+        self._directory = directory
+        self._parent = parent
+
+    def run(self):
+        raise NotImplementedError()
+
+    def upload(self, boto):
+        pass
+
+    def cleanUp(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def prepare(self):
+        pass
+
 class Schedule:
     def __init__(self, backup_dir, output_dir, retention_period):
         self._backup_dir = backup_dir
